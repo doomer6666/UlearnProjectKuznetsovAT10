4ef5eb7a15e5f3c7fec4e657defd343163a7ee5c
Evgeni <qazplm2125a@gmail.com>
2024-12-30 18:27:54 +0500

    create latest_vacancies.html

--- analytics/views.py
+++ analytics/views.py
@@ -1,10 +1,12 @@
+import requests
+import aiohttp
+import asyncio
 import re
-from django.db.models.functions import Substr, Round
 from django.shortcuts import render
-from .models import Vacancy, Currency, SalaryByYear, VacanciesCountByYear, SalaryByCity, VacanciesCountByCity, Skill
+from .models import Vacancy, SalaryByYear, VacanciesCountByYear, SalaryByCity, VacanciesCountByCity, Skill
 from django.db.models import Avg, Count, F, Q
 from collections import Counter
-
+from datetime import datetime, timedelta
 
 def index(request):
     return render(request, 'analytics/index.html')
@@ -114,10 +116,70 @@ def skills(request):
     })
 
 
+async def fetch_details(session, url):
+    async with session.get(url) as response:
+        return await response.json()
 
 
+def format_date(date_str):
+    date_obj = datetime.fromisoformat(date_str.replace('Z', '+00:00'))
+    return date_obj.strftime('%d %B %Y, %H:%M')
 
 
+def clean_description(description):
+    # Удаляем все HTML теги
+    cleanr = re.compile('<p>')
+    cleantext = re.sub(cleanr, '', description)
+    # Убираем лишние пробелы и стандартизируем текст
+    cleantext = re.sub(r'\s+', ' ', cleantext)
+    return cleantext.strip()
 
-def latest_vacancies(request):
-    return render(request, 'analytics/latest_vacancies.html')
+
+async def get_recent_jobs(profession_queries):
+    url = 'https://api.hh.ru/vacancies'
+    params = {
+        'date_from': (datetime.now() - timedelta(days=1)).isoformat(),
+        'per_page': 10,
+        'order_by': 'publication_time'
+    }
+
+    async with aiohttp.ClientSession() as session:
+        jobs = []
+        tasks = []
+
+        for profession in profession_queries:
+            params['text'] = profession
+            async with session.get(url, params=params) as response:
+                data = await response.json()
+
+            for item in data.get('items', []):
+                if any(profession.lower() in item['name'].lower() for profession in profession_queries):
+                    tasks.append(fetch_details(session, item['url']))
+
+        details = await asyncio.gather(*tasks)
+        for detail_data in details:
+            job = {
+                'name': detail_data['name'],
+                'company': detail_data['employer']['name'] if detail_data.get('employer') else 'Не указано',
+                'salary': detail_data['salary']['from'] if detail_data.get('salary') else 'Не указано',
+                'region': detail_data['area']['name'],
+                'published_at': format_date(detail_data['published_at']),
+                'description': clean_description(detail_data.get('description', 'Нет описания')),
+                'skills': ', '.join([skill['name'] for skill in detail_data.get('key_skills', [])])
+            }
+            jobs.append(job)
+
+    unique_jobs = {(job['name'], job['company']): job for job in jobs}.values()
+    return list(unique_jobs)
+
+
+async def latest_vacancies(request):
+    profession_queries = [
+        'Системный администратор', 'system admin', 'сисадмин', 'сис админ',
+        'системный админ', 'cистемный админ', 'администратор систем', 'системний адміністратор'
+    ]
+
+    jobs = await get_recent_jobs(profession_queries)
+
+    return render(request, 'analytics/latest_vacancies.html', {'jobs': jobs[:10]})
+
