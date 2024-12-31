f420211e8c424c3649d66916d232e483af1067ac
Evgeni <qazplm2125a@gmail.com>
2024-12-31 14:50:05 +0500

    refactor html and update statistics

--- analytics/views.py
+++ analytics/views.py
@@ -1,74 +1,49 @@
-import requests
 import aiohttp
 import asyncio
 import re
 from django.shortcuts import render
-from .models import Vacancy, SalaryByYear, VacanciesCountByYear, SalaryByCity, VacanciesCountByCity, Skill
-from django.db.models import Avg, Count, F, Q
-from collections import Counter
+from .models import SalaryByYear, VacanciesCountByYear, SalaryByCity, VacanciesCountByCity, Skill
 from datetime import datetime, timedelta
+
+
+def get_salary_and_vacancy_data():
+    salary_trends = SalaryByYear.objects.all()
+    vacancy_trends = VacanciesCountByYear.objects.all()
+    return salary_trends, vacancy_trends
+
+def get_skills_data():
+    skills_by_year = {}
+    images_by_year = {}
+
+    for skill in Skill.objects.all().order_by('year'):
+        if skill.year not in skills_by_year:
+            skills_by_year[skill.year] = []
+        skills_by_year[skill.year].append(skill)
+
+    for year in skills_by_year:
+        skills_by_year[year].sort(key=lambda x: x.count, reverse=True)
+        images_by_year[year] = f'img/skills_{year}_plot.png'
+
+    return skills_by_year, images_by_year
+
 
 def index(request):
     return render(request, 'analytics/index.html')
 
 
 def statistics(request):
-    # Получаем все вакансии
-    vacancies = Vacancy.objects.all()
-
-    # Динамика уровня зарплат по годам
-    salary_trends = (
-        vacancies.values('published_at__year')
-        .annotate(avg_salary=Avg(F('salary_from') + F('salary_to')) / 2)
-        .order_by('published_at__year')
-    )
-
-    # Динамика количества вакансий по годам
-    vacancy_trends = (
-        vacancies.values('published_at__year')
-        .annotate(count=Count('id'))
-        .order_by('published_at__year')
-    )
-
-    # Уровень зарплат по городам с конвертацией в рубли
-    salary_by_area = (
-        vacancies.values('area_name')
-        .annotate(avg_salary=Avg((F('salary_from') + F('salary_to')) / 2))
-        .order_by('-avg_salary')
-    )
-
-    # Доля вакансий по городам
-    vacancies_by_area = (
-        vacancies.values('area_name')
-        .annotate(count=Count('id'))
-        .order_by('-count')
-    )
-
-    # ТОП-20 навыков по годам
-    skills_by_year = {}
-
-    for vacancy in vacancies:
-        year = vacancy.published_at.year  # Получаем год публикации вакансии
-        skills = vacancy.key_skills.split(',')  # Предполагается, что навыки разделены запятыми
-
-        if year not in skills_by_year:
-            skills_by_year[year] = []
-
-        skills_by_year[year].extend([skill.strip() for skill in skills])  # Добавляем навыки в список
-
-    top_skills_by_year = {}
-
-    for year, skills in skills_by_year.items():
-        skill_counts = Counter(skills)  # Подсчет частоты навыков
-        top_skills = skill_counts.most_common(20)  # Получение ТОП-20 навыков
-        top_skills_by_year[year] = top_skills
+    salary_trends, vacancy_trends = get_salary_and_vacancy_data()
+    salary_by_city = SalaryByCity.objects.all()
+    vacancy_count_by_city = VacanciesCountByCity.objects.all()
+    skills_by_year, images_by_year = get_skills_data()
 
     context = {
         'salary_trends': salary_trends,
         'vacancy_trends': vacancy_trends,
-        'salary_by_area': salary_by_area,
-        'vacancies_by_area': vacancies_by_area,
-        'top_skills_by_year': top_skills_by_year,
+        'salary_by_area': salary_by_city,
+        'vacancies_by_area': vacancy_count_by_city,
+        'top_skills_by_year': skills_by_year,
+        'images_by_year': images_by_year,
     }
 
     return render(request, 'analytics/statistics.html', context)
@@ -77,10 +52,12 @@ def statistics(request):
 def demand(request):
     salary_trends = SalaryByYear.objects.all()
     vacancy_trends = VacanciesCountByYear.objects.all()
+
     context = {
         'salary_trends': salary_trends,
         'vacancy_trends':vacancy_trends,
     }
+
     return render(request, 'analytics/demand.html', context)
 
 
@@ -97,23 +74,14 @@ def geography(request):
 
 
 def skills(request):
-    skills_by_year = {}
-    images_by_year = {}
+    skills_by_year, images_by_year = get_skills_data()
 
-    for skill in Skill.objects.all().order_by('year'):
-        if skill.year not in skills_by_year:
-            skills_by_year[skill.year] = []
-        skills_by_year[skill.year].append(skill)
-
-    # Сортировка навыков по количеству в каждом году
-    for year in skills_by_year:
-        skills_by_year[year].sort(key=lambda x: x.count, reverse=True)
-        images_by_year[year] = f'img/skills_{year}_plot.png'  # Путь к изображению
-
-    return render(request, 'analytics/skills.html', {
+    context = {
         'top_skills_by_year': skills_by_year,
         'images_by_year': images_by_year
-    })
+    }
+
+    return render(request, 'analytics/skills.html', context)
 
 
 async def fetch_details(session, url):
@@ -123,11 +91,11 @@ async def fetch_details(session, url):
 
 def format_date(date_str):
     date_obj = datetime.fromisoformat(date_str.replace('Z', '+00:00'))
-    return date_obj.strftime('%d %B %Y, %H:%M')
+    return date_obj.strftime('%d.%m.%Y, %H:%M')
 
 
 def clean_description(description):
-    # Удаляем все HTML теги
+    # Удаляем все <p> HTML теги
     cleanr = re.compile('<p>')
     cleantext = re.sub(cleanr, '', description)
     # Убираем лишние пробелы и стандартизируем текст
@@ -158,10 +126,14 @@ async def get_recent_jobs(profession_queries):
 
         details = await asyncio.gather(*tasks)
         for detail_data in details:
+            salary_from = detail_data['salary']['from'] if detail_data.get('salary') else None
+            currency = detail_data['salary']['currency'] if detail_data.get('salary') else 'RUR'
+            salary_in_rub = f"{salary_from} {currency}" if salary_from else 'Не указано'
+
             job = {
                 'name': detail_data['name'],
                 'company': detail_data['employer']['name'] if detail_data.get('employer') else 'Не указано',
-                'salary': detail_data['salary']['from'] if detail_data.get('salary') else 'Не указано',
+                'salary': salary_in_rub,
                 'region': detail_data['area']['name'],
                 'published_at': format_date(detail_data['published_at']),
                 'description': clean_description(detail_data.get('description', 'Нет описания')),
